05d8ff05e3337ad92840dbe0457efa1f8fca9914
Shamil Frontend <sh.alisultanov@maximatelecom.ru>
2020-12-26 16:09:39 +0300

    base webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,14 @@
+const path = require('path')
+
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
+
+  mode: 'development',
+
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
